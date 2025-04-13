692b51da7e13fdce527d4e7fd7cd4a1f30bfa8e9
SuprithiVoyant <suprithh@ivoyant.com>
2025-04-14 02:44:10 +0530

    microervice

---
+++ question-service/src/main/java/com/question/question_service/entity/Question.java
@@ -0,0 +1,21 @@
+package com.question.question_service.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private  Long questionId;
+    private  String question;
+    private  Long quizId;
+}
---
+++ quiz-service/src/main/java/com/quiz/quiz_service/repository/QuizRepository.java
@@ -0,0 +1,8 @@
+package com.quiz.quiz_service.repository;
+
+import com.quiz.quiz_service.entity.Quiz;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
+
+}
